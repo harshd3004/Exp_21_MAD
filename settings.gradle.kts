1d81c311978845a3c13c5d83daba049c796eb2a6
lenovo 03 <desaiharsh512@gmail.com>
2024-02-02 15:43:45 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Exp_21_MAD"
+include(":app")
